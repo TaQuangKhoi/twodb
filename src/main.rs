4b39d7df49164cfb41090b485240ed0436da716d
Tạ Quang Khôi <khoi0941@gmail.com>
2024-08-09 00:10:38 +0700

    Use keyword match to print raw variable instead of Some(x)
    # Conflicts:
    #	src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,32 @@ use postgres::{Client, Error, NoTls};
 fn main() {
     let mut client = connect().unwrap();
     let query = "___YOUR_QUERY_HERE___";
-    let rows = client.query(query, &[]).unwrap();
+    let rows = client.query(
+        query,
+        &[],
+    ).unwrap();
 
     for row in rows {
-        let id: Option<i64> = row.try_get(0).unwrap_or(None);
+        let id: Option<i64> = row.try_get(0).unwrap_or(None); // bigserial
         let name: Option<&str> = row.try_get(1).unwrap_or(None);
         let email: Option<&str> = row.try_get(2).unwrap_or(None);
 
-        println!("id: {:?}, name: {:?}, email: {:?}", id, name, email);
+        let mut raw_id: i64 = None.unwrap_or(0);
+        let mut raw_name: &str = None.unwrap_or("None");
+        let mut raw_email: &str = None.unwrap_or("None");
+        match id {
+            Some(id) => raw_id = id,
+            None => raw_id = 0,
+        }
+        match name {
+            Some(name) => raw_name = name,
+            None => raw_name = "None",
+        }
+        match email {
+            Some(email) => raw_email = email,
+            None => raw_email = "None",
+        }
+        println!("id: {}, name: {}, email: {}", raw_id, raw_name, raw_email)
     }
 }
 
@@ -23,7 +41,7 @@ fn connect() -> Result<Client, Error> {
 
     let client = Client::connect(
         database_url.as_str(),
-        NoTls
+        NoTls,
     )?;
 
     Ok(client)
