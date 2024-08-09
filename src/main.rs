f5151356096c327baa5b1b608de01ccec5e40462
Tạ Quang Khôi <khoi0941@gmail.com>
2024-08-09 15:08:47 +0700

    Save clean tables to sqlite

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use postgres::{Client, Error, NoTls};
 use std::env::var;
 use std::time::SystemTime;
+use rusqlite::{params, Connection, Result};
 
 fn main() {
     let source_database_name = var("POSTGRES_DB_1").unwrap_or(String::from(""));
@@ -9,9 +10,16 @@ fn main() {
     get_clean_tables(source_database_name);
 }
 
+struct Table {
+    id: i32,
+    name: String,
+    table_type: String,
+    export_complexity_type: String,
+}
+
 fn get_clean_tables(database_name: String) {
     let mut client = connect(database_name).unwrap();
-    let query ="SELECT table_name \
+    let query = "SELECT table_name \
         FROM information_schema.tables \
         WHERE table_schema = 'public' \
         AND table_type = 'BASE TABLE' \
@@ -27,8 +35,35 @@ fn get_clean_tables(database_name: String) {
         &[],
     ).unwrap();
 
+    let conn = Connection::open("twodb.db").unwrap();
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS tables (
+            id INTEGER PRIMARY KEY,
+            name TEXT NOT NULL,
+            table_type TEXT NOT NULL,
+            export_complexity_type TEXT NOT NULL
+        )",
+        params![],
+    ).unwrap();
+
     for row in rows {
-        println!("{}", row_to_string(row));
+        let table = Table {
+            id: 0,
+            name: row.get(0),
+            table_type: String::from("BASE TABLE"),
+            export_complexity_type: "SIMPLE".to_string(),
+        };
+
+        // check if table exists
+        let mut stmt = conn.prepare("SELECT id FROM tables WHERE name = ?1").unwrap();
+        let mut rows = stmt.query(params![table.name]).unwrap();
+
+        if rows.next().unwrap_or(None).is_none() {
+            conn.execute(
+                "INSERT INTO tables (name, table_type , export_complexity_type) VALUES (?1, ?2, ?3)",
+                params![table.name, table.table_type, table.export_complexity_type],
+            ).unwrap();
+        }
     }
 }
 
