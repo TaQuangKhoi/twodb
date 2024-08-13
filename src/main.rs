c3b1a5a49f38fb43775a9aefc77b6b6e79f89559
Tạ Quang Khôi <khoi0941@gmail.com>
2024-08-13 16:26:18 +0700

    Clean fn main

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,4 @@
-pub mod database;
-mod preparation;
-mod table;
-mod core;
-mod working_database;
-
-use database::connect;
-use std::env::var;
-use postgres::error::SqlState;
-use rusqlite::{Connection};
-use crate::core::get_tables;
-use crate::preparation::prepare_knowledge;
-use crate::working_database::get_cells;
-
 fn main() {
-    prepare_knowledge();
-    compare_database();
-    // export based on complexity
+
 }
 
