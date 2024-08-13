3c1a77cf39b13dc194819d1d64ca84efb8adc73f
Tạ Quang Khôi <khoi0941@gmail.com>
2024-08-13 16:21:40 +0700

    Move compare_database and _row_to_string out of main.rs

--- src/working_database.rs
+++ src/working_database.rs
@@ -5,6 +5,68 @@ use crate::_row_to_string;
 use crate::database::connect;
 use crate::table::{build_base_simple_table, create_tables_table, insert_new_table};
 
+fn compare_database() {
+    let source_database_name = var("POSTGRES_DB_1").unwrap_or(String::from(""));
+    let target_database_name = var("POSTGRES_DB_2").unwrap_or(String::from(""));
+    let mut source_client = connect(source_database_name.clone()).unwrap();
+    let mut target_client = connect(target_database_name.clone()).unwrap();
+
+    let sqlite_conn = Connection::open("twodb.db").unwrap();
+    let tables_to_compare = get_tables(&sqlite_conn);
+
+    for table in tables_to_compare {
+        let table_name = table.name.clone();
+        let query = "SELECT * FROM ".to_string() + table_name.as_str();
+        let source_rows = match source_client.query(&query, &[]) {
+            Ok(rows) => rows,
+            Err(err) => {
+                if let Some(db_err) = err.as_db_error() {
+                    if db_err.code() == &SqlState::from_code("42P01") {
+                        println!("Table: {} does not exist in the source database", table_name);
+                        continue;
+                    }
+                }
+                panic!("Error querying source database: {:?}", err);
+            }
+        };
+        let target_rows = match target_client.query(&query, &[]) {
+            Ok(rows) => rows,
+            Err(err) => {
+                if let Some(db_err) = err.as_db_error() {
+                    if db_err.code() == &SqlState::from_code("42P01") {
+                        println!("Table: {} does not exist in the target database", table_name);
+                        continue;
+                    }
+                }
+                panic!("Error querying target database: {:?}", err);
+            }
+        };
+
+        let source_rows_count = source_rows.len();
+        let target_rows_count = target_rows.len();
+        if source_rows_count != target_rows_count {
+            println!("Table: {} has different rows count: {} vs {}", table_name, source_rows_count, target_rows_count);
+            continue;
+        }
+
+        for (source_row, target_row) in source_rows.iter().zip(target_rows.iter()) {
+            let source_cells = get_cells(source_row);
+
+            let target_cells = get_cells(target_row);
+
+            if source_cells != target_cells {
+                println!("Table: {} has different cells", table_name);
+                println!("Source: {:?}", source_cells);
+                println!("Target: {:?}", target_cells);
+            }
+        }
+    }
+}
+
+fn _row_to_string(row: &postgres::Row) -> String {
+    format!("{:?}", get_cells(row))
+}
+
 pub fn get_cells(row: &postgres::Row) -> Vec<String> {
     let columns = row.columns();
     let cells: Vec<String> = columns.iter().map(|column| {
