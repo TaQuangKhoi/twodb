2f4bc27a11d912097f9a0c5c478f2a916cb01911
Tạ Quang Khôi <khoi0941@gmail.com>
2024-08-13 16:30:43 +0700

    Add dependencies and rename app

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ fn main() -> eframe::Result {
     eframe::run_native(
         "eframe template",
         native_options,
-        Box::new(|cc| Ok(Box::new(eframe_template::TemplateApp::new(cc)))),
+        Box::new(|cc| Ok(Box::new(twodb::TemplateApp::new(cc)))),
     )
 }
 
@@ -37,7 +37,7 @@ fn main() {
             .start(
                 "the_canvas_id",
                 web_options,
-                Box::new(|cc| Ok(Box::new(eframe_template::TemplateApp::new(cc)))),
+                Box::new(|cc| Ok(Box::new(twodb::TemplateApp::new(cc)))),
             )
             .await;
 
