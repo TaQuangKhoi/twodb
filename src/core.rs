ff0ffe4d3bcd9fabaf29dd9108a94b1bc08694d1
Tạ Quang Khôi <khoi0941@gmail.com>
2024-08-14 17:00:04 +0700

    Update core get_tables

--- src/core.rs
+++ src/core.rs
@@ -1,12 +1,24 @@
 /*! This file contains the core logic of the application. */
 
 use rusqlite::{Connection, params};
+use crate::database::connect;
 use crate::table::{ExportComplexityType, Table, TableType};
 
 /// Get all tables from the SQLite database
-pub fn get_tables(conn: &Connection) -> Vec<Table> {
-    let mut stmt = conn.prepare(
-        "SELECT id, name, table_type, export_complexity_type, database, export_order FROM tables"
+pub fn get_tables() -> Vec<Table> {
+    let sqlite_conn = Connection::open("twodb.db").unwrap();
+    let mut stmt = sqlite_conn.prepare(
+        "SELECT
+            id,
+            name,
+            table_type,
+            export_complexity_type,
+            database,
+            export_order,
+            is_self_referencing,
+            self_referencing_column,
+            row_count
+        FROM tables"
     ).unwrap();
     let tables = stmt.query_map(params![], |row| {
         Ok(Table {
@@ -17,7 +29,8 @@ pub fn get_tables(conn: &Connection) -> Vec<Table> {
             database: row.get(4)?,
             export_order: row.get(5)?,
             is_self_referencing: false,
-            row_count: 0,
+            self_referencing_column: String::from(""),
+            row_count: row.get(8)?,
         })
     }).unwrap();
     let mut result = Vec::new();
