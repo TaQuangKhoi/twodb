258fdb9d2c22ad25ecf48d5cd111603d08c0b5d9
Tạ Quang Khôi <khoi0941@gmail.com>
2024-08-19 17:18:07 +0700

    Update fn notify

--- src/app_fn_impl.rs
+++ src/app_fn_impl.rs
@@ -1,9 +1,10 @@
 use std::sync::{Arc, Mutex};
+use log::info;
 use crate::TwoDBApp;
 
 impl TwoDBApp {
     pub fn notify(text: String, is_busy: Arc<Mutex<bool>>, toast_text: Arc<Mutex<String>>) {
-        println!("{}", text);
+        info!("{}", text);
         *is_busy.lock().unwrap() = false;
         *toast_text.lock().unwrap() = text;
     }
