b8cd1392a7553ef87a3c2001f3f4b6a78faa2708
Tạ Quang Khôi <khoi0941@gmail.com>
2024-08-19 12:56:47 +0700

    Update fn move_one_table

--- src/action/move_data.rs
+++ src/action/move_data.rs
@@ -1,32 +1,20 @@
 use std::any::Any;
 use std::env::var;
 use std::collections::HashMap;
-use crate::action::working_database::{get_cells, get_rows};
+use postgres::Row;
+use crate::action::working_database::{get_cell_value_by_column_name, get_rows};
 
 pub fn move_one_table(table_name: String) {
     let source_database_name = var("POSTGRES_DB_SOURCE").unwrap_or(String::from(""));
     let target_database_name = var("POSTGRES_DB_TARGET").unwrap_or(String::from(""));
 
     // STEP 1: Get data of table from source database
-    let mut data: Vec<HashMap<String, Box<dyn Any>>> = Vec::new();
-    let source_rows = get_rows(source_database_name, table_name.clone());
-    for source_row in &source_rows {
-        let columns = source_row.columns();
-        let cells = get_cells(&source_row);
-        let mut row: HashMap<String, Box<dyn Any>> = HashMap::new();
-        for (i, cell) in cells.iter().enumerate() {
-            let column_name = columns[i].name();
-            println!("Column name: {}", column_name);
-            row.insert(column_name.parse().unwrap(), Box::new(cell.clone()));
-        }
-        data.push(row);
-    }
+    let source_rows: Vec<Row> = get_rows(source_database_name, table_name.clone());
 
     // STEP 2: Insert data into target database
-    for row in data {
-        for (column_name, cell) in row {
-            let value = cell.downcast_ref::<String>().unwrap();
-            println!("{}", value);
-        }
+    for source_row in source_rows.clone() {
+        let columns = source_row.columns();
+        let id: String = get_cell_value_by_column_name(source_row, "number".to_string());
+        println!("ID: {:?}", id);
     }
 }
