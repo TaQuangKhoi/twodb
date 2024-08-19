15cd1b705c8369fee3a2a495bf5ea01f3995053a
Tạ Quang Khôi <khoi0941@gmail.com>
2024-08-19 13:49:57 +0700

    Update fn move_one_table, add fn build_insert_query

--- src/action/move_data.rs
+++ src/action/move_data.rs
@@ -1,7 +1,5 @@
-use std::any::Any;
 use std::env::var;
-use std::collections::HashMap;
-use postgres::Row;
+use postgres::{Column, Row};
 use crate::action::working_database::{get_cell_value_by_column_name, get_rows};
 
 pub fn move_one_table(table_name: String) {
@@ -9,12 +7,21 @@ pub fn move_one_table(table_name: String) {
     let target_database_name = var("POSTGRES_DB_TARGET").unwrap_or(String::from(""));
 
     // STEP 1: Get data of table from source database
-    let source_rows: Vec<Row> = get_rows(source_database_name, table_name.clone());
+    let source_rows: Vec<Row> = get_rows(source_database_name, &table_name);
 
     // STEP 2: Insert data into target database
     for source_row in source_rows.clone() {
-        let columns = source_row.columns();
-        let id: String = get_cell_value_by_column_name(source_row, "number".to_string());
-        println!("ID: {:?}", id);
+        let columns: &[Column] = source_row.columns();
+        let query:String = build_insert_query(&table_name, columns, &source_row);
+        println!("Query: {:?}", query);
     }
+}
+
+fn build_insert_query(table_name: &String, columns: &[Column], row: &Row) -> String {
+    let columns_str = columns.iter().map(|c| c.name()).collect::<Vec<_>>().join(", ");
+    let values_str = columns.iter().map(
+        |c|
+        { format!("'{}'", get_cell_value_by_column_name(row, c.name().to_string())) }
+    ).collect::<Vec<_>>().join(", ");
+    format!("INSERT INTO {} ({}) VALUES ({})", table_name, columns_str, values_str)
 }
