e40fd17d58053b3be9f03a24b03cbd34dd0b834a
Tạ Quang Khôi <khoi0941@gmail.com>
2024-09-05 14:55:06 +0700

    refactor module move

--- src/action/mod.rs
+++ src/action/mod.rs
@@ -1,5 +1,5 @@
 pub mod update;
 pub mod working_database;
-pub mod move_data;
+pub mod r#move;
 
 pub const TWODB_NULL: &str = "twodb_NULL";
--- src/twoui/menu_bar/mod.rs
+++ src/twoui/menu_bar/mod.rs
@@ -5,7 +5,7 @@ use std::env::var;
 use std::thread;
 use egui::Align2;
 use log::info;
-use crate::action::move_data::{get_queries_one_table, move_one_table};
+use crate::action::r#move::{get_queries_one_table, move_one_table};
 use crate::core::get_knowledge::{get_tables_with_condition};
 /// Render the menu bar
 
